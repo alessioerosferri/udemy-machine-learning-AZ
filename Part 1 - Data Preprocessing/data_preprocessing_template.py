c2e00bc618a38f8ae2ea8d12ef97e4636a7b5237
Alessio <alessioferriweb@gmail.com>
2020-04-03 19:20:18 +0100

    splitting out encoded values in different columns so we don't give priority to countries as they are categorical values

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
@@ -15,6 +17,13 @@ y = dataset.iloc[:, 3].values
 imputer = SimpleImputer()
 imputer = imputer.fit(X[:, 1:])
 X[:, 1:] = imputer.transform(X[:, 1:])
+
+
+ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+
+
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
