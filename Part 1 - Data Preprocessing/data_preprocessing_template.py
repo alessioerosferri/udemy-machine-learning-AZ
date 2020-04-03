c7b7ffdea7155b68b798b428b206e89123a973c8
Alessio <alessioferriweb@gmail.com>
2020-04-03 18:53:27 +0100

    Using imputer to replace empty values with mean values

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -5,11 +5,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
+imputer = SimpleImputer()
+imputer = imputer.fit(X[:, 1:])
+X[:, 1:] = imputer.transform(X[:, 1:])
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
