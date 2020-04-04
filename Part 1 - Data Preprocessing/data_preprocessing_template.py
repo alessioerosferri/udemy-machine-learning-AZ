a399096f5b8d75e23a68d99a5d3a8134cf4bbf38
Alessio <alessioferriweb@gmail.com>
2020-04-04 16:52:34 +0100

    Building final template commenting out bits that we won't need for our ML model

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -14,22 +14,22 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-imputer = SimpleImputer()
-imputer = imputer.fit(X[:, 1:])
-X[:, 1:] = imputer.transform(X[:, 1:])
+# imputer = SimpleImputer()
+# imputer = imputer.fit(X[:, 1:])
+# X[:, 1:] = imputer.transform(X[:, 1:])
 
 
-ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-X = np.array(ct.fit_transform(X), dtype=np.float)
-labelEncoder_y = LabelEncoder()
-y = labelEncoder_y.fit_transform(y)[:, None]
+# ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+# X = np.array(ct.fit_transform(X), dtype=np.float)
+# labelEncoder_y = LabelEncoder()
+# y = labelEncoder_y.fit_transform(y)[:, None]
 
 
 
 # Splitting the dataset into the Training set and Test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Feature Scaling
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+# # Feature Scaling
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.transform(X_test)
